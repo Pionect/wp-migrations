aed65b4fba782d5e49736099c8930429182d7de5
Tomas van Rijsse <tomas@pionect.nl>
2016-07-01 23:26:12 +0200

    Add an example implementation

---
+++ examples/migrations/2016_05_19_1109_disable_search_engines.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Migrations;
+
+class DisableSearchEngines extends \WP_Migrations\Migrations\Types\OptionMigration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @param $option_value as returned from get_option
+     * @return void
+     */
+    public function up($option_value)
+    {
+        return $option_value;
+    }
+
+    public function get_option_name()
+    {
+        return 'active_plugins';
+    }
+
+    public function get_validation_rules()
+    {
+        return [
+            'plugin_exists'  => 'debug-bar',
+            'plugin_version' => '0.8.*'
+        ];
+    }
+}
---
+++ examples/migrations/2016_05_19_1309_test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Migrations;
+
+class Test
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @param $option_value as returned from get_option
+     * @return void
+     */
+    public function up()
+    {
+        //do something
+    }
+}
